a134662d619a83601b71549fe4cf5c4817e2b5ab
Zac Demi <zac.demi@gmail.com>
2019-12-14 20:08:27 -0600

    design changes and content added

--- application.py
+++ application.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, url_for
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def home():
     return render_template('home.html')
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+    application.debug = True
+    application.run()
 
 
 
